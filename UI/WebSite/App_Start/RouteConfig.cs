d2562f2d0fff82a3bd4e58db7fd12f7e06018765
Kirk Chen <rwk0119@yahoo.com.tw>
2013-10-01 20:22:12 +0800

    create sample controller for display sample coding structure

--- UI/WebSite/App_Start/RouteConfig.cs
+++ UI/WebSite/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ApiSample.UI.WebSite
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Sample", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ UI/WebSite/Controllers/SampleController.cs
@@ -0,0 +1,33 @@
+﻿using ApiSample.BL.Interfaces;
+using ApiSample.BL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ApiSample.UI.WebSite.Controllers
+{
+    public class SampleController : Controller
+    {
+        public ISampleService SampleService { get; set; }
+
+        public SampleController()
+            :this(new SampleService())
+        {
+        }
+
+        public SampleController(ISampleService sampleService)
+        {
+            this.SampleService = sampleService;
+        }
+
+        public ActionResult Index()
+        {
+            var data = this.SampleService.GetSamples();
+
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
