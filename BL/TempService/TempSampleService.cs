25524edcdbb2bda46a3a45ac75578a6068328183
Kirk Chen <rwk0119@yahoo.com.tw>
2013-10-02 16:19:24 +0800

    add tempsampleservice for emergency

---
+++ BL/TempService/TempSampleService.cs
@@ -0,0 +1,26 @@
+﻿using ApiSample.BL.Interfaces;
+using ApiSample.DA.Interfaces;
+using ApiSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiSample.BL.TempServices
+{
+    public class TempSampleService : ISampleService
+    {
+        public ISampleRepository SampleRepository { get; set; }        
+
+        public TempSampleService(ISampleRepository sampleRepository)
+        {
+            this.SampleRepository = sampleRepository;
+        }
+
+        public IEnumerable<SampleModel> GetSamples()
+        {
+            return this.SampleRepository.GetSamples().Take(3);
+        }
+    }
+}
