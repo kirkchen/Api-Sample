0cf5609b8c9d5e26568ffd02a8dfc9a90a2a3d37
Kirk Chen <rwk0119@yahoo.com.tw>
2013-10-13 16:32:08 +0800

    1. Integrate with elmah 2. Add elmah constraint

--- UI/WebSite/App_Start/RouteConfig.cs
+++ UI/WebSite/App_Start/RouteConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ApiSample.Utility.Extensions.Elmah;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,34 @@ namespace ApiSample.UI.WebSite
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            var namespaces = new[] { "Elmah.Mvc" };
+            var elmahRoute = "Errors";
+            string allowedIps = "::1;127.0.0.1";
+
+            routes.MapRoute(
+                "Elmah.Override",
+                string.Format("{0}/{{resource}}", elmahRoute),
+                new
+                {
+                    controller = "Elmah",
+                    action = "Index",
+                    resource = UrlParameter.Optional
+                },
+                new { resource = new IPConstraint(allowedIps) },
+                namespaces);
+
+            routes.MapRoute(
+                "Elmah.Override.Detail",
+                string.Format("{0}/detail/{{resource}}", elmahRoute),
+                new
+                {
+                    controller = "Elmah",
+                    action = "Detail",
+                    resource = UrlParameter.Optional
+                },
+                new { resource = new IPConstraint(allowedIps) },
+                namespaces);
 
             routes.MapRoute(
                 name: "Default",
